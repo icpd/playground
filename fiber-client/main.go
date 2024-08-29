965507c5e5067eb14287610b066218a7aa0a6f83
icpd <35096485+icpd@users.noreply.github.com>
2024-08-29 17:37:06 +0800

    feat: add Fiber HTTP client example
    Added initial implementation of a Fiber HTTP client with basic GET requests and custom header usage. Included necessary dependencies in go.mod and go.sum.

---
+++ fiber-client/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	"github.com/gofiber/fiber/v3/client"
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	c := client.New().SetDial(func(addr string) (net.Conn, error) {
+		fmt.Println("dial raw addr", addr)
+		addr = "127.0.0.1:9091" // proxyman addr
+		return fasthttp.Dial(addr)
+	})
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		resp, err := c.SetHeader("X-Client", "gogo").
+			Get("http://baidu.com:123", client.Config{Header: map[string]string{"Host": "github.com"}}) // host 不会生效，会被 URL 中的地址覆盖掉
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(resp)
+	}()
+
+	go func() {
+		defer wg.Done()
+		resp, err := c.Get("http://google.com")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(resp)
+	}()
+	wg.Wait()
+
+	// 自己创建 request 来修改 host
+	req := client.AcquireRequest()
+	defer client.ReleaseRequest(req)
+
+	req.RawRequest.UseHostHeader = true // 没有这个就 Host 就会被重新覆盖
+	req.SetHeader("Host", "baidu.com:110")
+	req.SetClient(c)
+	resp, err := req.Get("http://baidu.com:321")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Close()
+	fmt.Println(resp.String())
+}
