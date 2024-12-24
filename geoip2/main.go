b00c0ad57fcc0480b9fa4b1e57039dd888dfff64
icpd <35096485+icpd@users.noreply.github.com>
2024-12-24 17:39:09 +0800

    feat: geoip2

--- geoip2/main.go
+++ geoip2/main.go
@@ -2,25 +2,66 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 
 	"github.com/oschwald/geoip2-golang"
 )
 
 func main() {
-	// https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb
-	db, err := geoip2.Open("Country.mmdb")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	ip := net.ParseIP("103.116.72.17")
 
-	ip := net.ParseIP("38.9.147.134")
-	record, err := db.Country(ip)
+	countryDB, err := geoip2.Open("Country.mmdb")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+	defer countryDB.Close()
+
+	co, err := countryDB.Country(ip)
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Println("country", record.Country)
+	fmt.Println("CountryDB")
+	fmt.Println("country", co.Country)
+	fmt.Println()
+
+	cityDB, err := geoip2.Open("City.mmdb")
+	if err != nil {
+		panic(err)
+	}
+	defer cityDB.Close()
+
+	ci, err := cityDB.City(ip)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("CityDB")
+	fmt.Println("country", ci.Country)
+	fmt.Println("city", ci.City)
+	fmt.Println()
+
+	asnDB, err := geoip2.Open("ASN.mmdb")
+	if err != nil {
+		panic(err)
+	}
+	defer asnDB.Close()
+
+	asn, err := asnDB.ASN(ip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("ASNDB")
+	fmt.Println("ASN", asn)
+
+	// output
+	// CountryDB
+	// country {map[de:Hongkong en:Hong Kong es:Hong Kong fr:Hong Kong ja:香港 pt-BR:Hong Kong ru:Гонконг zh-CN:香港] HK 1819730 false}
+	//
+	// CityDB
+	// country {map[de:Hongkong en:Hong Kong es:Hong Kong fr:Hong Kong ja:香港 pt-BR:Hong Kong ru:Гонконг zh-CN:香港] HK 1819730 false}
+	// city {map[en:Kowloon City zh-CN:九龙城] 1819607}
+	//
+	// ASNDB
+	// ASN &{PRIME-SEC 400618}
 }
